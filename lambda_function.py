4976c97344b1087687bd7b3892fbdc341d22637a
Tom Lynch <twhlynch@gmail.com>
2024-06-05 18:22:56 +1000

    aws test script

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,31 @@
 import json
+import requests
 import boto3
 
-def lambda_handler(event, context):
-    data = {
-        "test": "test2"
-    }
-    json_data = json.dumps(data)
-    
-    s3 = boto3.client('s3')
-    bucket_name = 'grab-tools-stats'
+SERVER_URL = "https://api.slin.dev/grab/v1/"
+PAGE_URL = "https://grab-tools.live/"
+VIEWER_URL = "https://grabvr.quest/levels/viewer/"
+FORMAT_VERSION = "100"
+S3 = boto3.client('s3')
+BUCKET_NAME = 'grab-tools-stats'
 
-    file_name = 'data.json'
-    s3.put_object(Bucket=bucket_name, Key=file_name, Body=json_data)
+def write_json_file(filename, data):
+    S3.put_object(Bucket=BUCKET_NAME, Key=filename, Body=data)
+
+def get_all_verified(stamp=''):
+    verified = []
+    while True:
+        url = f"{SERVER_URL}list?max_format_version={FORMAT_VERSION}&type=ok&page_timestamp={stamp}"
+        data = requests.get(url).json()
+        verified.extend(data)
+        if "page_timestamp" in data[-1]:
+            stamp = data[-1]["page_timestamp"]
+        else:
+            break
+    return verified
+
+def lambda_handler(event, context):
+    write_json_file('all_verified.json', get_all_verified())
 
     return {
         'statusCode': 200,
