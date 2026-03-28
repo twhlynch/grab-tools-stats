97d6ec9ee28102013ac733bcd4871051e5b0e0fa
Tom Lynch <twhlynch@gmail.com>
2026-03-29 05:17:08 +1100

    refactor clean up records script more

--- scripts/records.py
+++ scripts/records.py
@@ -20,30 +20,28 @@ def get_level_leaderboard(identifier):
 
 
 class Scope:
+    # fmt: off
     def __init__(self):
         # rating -> user id -> {maps, user_name}
-        self.difficulty_records = {rating: {} for rating in utils.RATINGS}
+        self.difficulty_records: dict[str, dict[str, dict[str, str]]] = {rating: {} for rating in utils.RATINGS}
         # rating -> level count
-        self.difficulty_lengths = {rating: 0 for rating in utils.RATINGS}
-        # user id -> [record count, ['title|identifier'][], username]
-        self.leaderboard = {}
+        self.difficulty_lengths: dict[str, int] = {rating: 0 for rating in utils.RATINGS}
+        # user id -> [record count, identifier[], username]
+        self.leaderboard: dict[str, list[int | list[str] | str]] = {}
         # {...level, leaderboard}[]
-        self.sole_victors = []
+        self.sole_victors: list[dict] = []
         # user id -> [finish count, username, total time]
-        self.user_finishes = {}
-        # user id -> [username, firsts count]
-        self.first_to_beat = {}
+        self.user_finishes: dict[str, list[int | str | float]] = {}
         # user id -> timestamp[]
-        self.timestamps_data = {}
-        # 'user id|latest|oldest'[]
-        self.timestamps_data_result = []
-        # user id -> [record count, ['title|identifier'][], username]
-        self.sorted_leaderboard = {}
+        self.timestamps_data: dict[str, list[int]] = {}
+        # 'user id:latest:oldest'[]
+        self.timestamps_data_result: list[str] = []
+        # user id -> [record count, identifier[], username]
+        self.sorted_leaderboard: dict[str, list[int, list[str], str]] = {}
 
 
 def process(level: dict, scope: Scope):
     identifier = level["identifier"]
-    title = level.get("title", "")
     statistics = level.get("statistics", {})
 
     leaderboard_data = get_level_leaderboard(identifier)
@@ -60,35 +58,33 @@ def process(level: dict, scope: Scope):
     if length == 1:
         scope.sole_victors.append(level)
 
-    # add record holder
-    record_holder = leaderboard_data[0]["user_id"]
-    if record_holder not in scope.leaderboard:
-        scope.leaderboard[record_holder] = [0, [], leaderboard_data[0]["user_name"]]
+    # get record holder
+    first_entry = leaderboard_data[0]
+    record_user_name = first_entry["user_name"]
+    record_user_id = first_entry["user_id"]
 
-    scope.leaderboard[record_holder][0] += 1
-    scope.leaderboard[record_holder][1].append(
-        [title + "|" + identifier]
-    )  # TODO: why did i add the title here
+    # add record holder
+    if record_user_id not in scope.leaderboard:
+        scope.leaderboard[record_user_id] = [0, [], record_user_name]
+
+    # accumulate stats
+    scope.leaderboard[record_user_id][0] += 1
+    scope.leaderboard[record_user_id][1].append(identifier)
 
     # difficulty
-    difficulty_string = (
-        statistics["difficulty_string"]
-        if "difficulty_string" in statistics
-        else "unrated"
-    )
+    difficulty_string = statistics.get("difficulty_string", "unrated")
     scope.difficulty_lengths[difficulty_string] += 1
 
     # finishes
     for record in leaderboard_data:
-        user_id = record["user_id"] if "user_id" in record else None
-        timestamp = record["timestamp"] if "timestamp" in record else None
-
-        if user_id is None:
-            continue
+        user_id = record["user_id"]
+        user_name = record["user_name"]
+        timestamp = record.get("timestamp", None)
+        best_time = record.get("best_time", 0)
 
         # timestamp data
-        if timestamp is not None:
-            timestamp_id = int(int(timestamp) / 100)
+        if timestamp:
+            timestamp_id = int(timestamp) // 100
             if user_id not in scope.timestamps_data:
                 scope.timestamps_data[user_id] = [timestamp_id]
             else:
@@ -99,60 +95,35 @@ def process(level: dict, scope: Scope):
         if user_id not in diff_records:
             diff_records[user_id] = {
                 "maps": 0,
-                "user_name": record["user_name"],
+                "user_name": user_name,
             }
+
         diff_records[user_id]["maps"] += 1
-
-    # first person to complete (in top 100)
-    first_record = None
-    first_timestamp = float("inf")  # wtf is this syntax
-
-    # all user stats
-    for record in leaderboard_data:
-        current_timestamp = record["timestamp"]
-        user_id = record["user_id"]
-
-        if int(current_timestamp) < first_timestamp:
-            first_timestamp = int(current_timestamp)
-            first_record = record
 
         # total finishes
         if user_id not in scope.user_finishes:
-            scope.user_finishes[user_id] = [0, record["user_name"], 0]
+            scope.user_finishes[user_id] = [0, user_name, 0]
+
         scope.user_finishes[user_id][0] += 1
-
-        try:
-            scope.user_finishes[user_id][2] += (
-                record["best_time"] if "best_time" in record else 0
-            )
-        except TypeError:  # FIXME: what caused this?
-            print(scope.user_finishes[user_id])
-
-    # first person to complete
-    if first_record is not None:
-        first_user_id = first_record["user_id"]
-        first_user_name = first_record["user_name"]
-
-        if first_user_id not in scope.first_to_beat:
-            scope.first_to_beat[first_user_id] = [first_user_name, 0]
-        scope.first_to_beat[first_user_id][1] += 1
+        scope.user_finishes[user_id][2] += best_time
 
 
 def sanitize(scope: Scope):
-    leaderboard = {
-        user: scores for user, scores in scope.leaderboard.items() if scores[0] >= 10
-    }
+    # users with more that 10 records
+    scope.leaderboard = {k: v for k, v in scope.leaderboard.items() if v[0] >= 10}
+    # sort by records descending
     scope.sorted_leaderboard = dict(
-        sorted(leaderboard.items(), key=lambda x: x[1][0], reverse=True)
+        sorted(scope.leaderboard.items(), key=lambda x: x[1][0], reverse=True)
     )
 
     for difficulty in scope.difficulty_records:
+        # users with more than 10 records
         scope.difficulty_records[difficulty] = {
-            user_key: records
-            for user_key, records in scope.difficulty_records[difficulty].items()
-            if records["maps"] >= 10
+            k: v
+            for k, v in scope.difficulty_records[difficulty].items()
+            if v["maps"] >= 10
         }
-
+        # sort by records descending top 200
         scope.difficulty_records[difficulty] = dict(
             sorted(
                 scope.difficulty_records[difficulty].items(),
@@ -161,34 +132,30 @@ def sanitize(scope: Scope):
             )[:200]
         )
 
+    # timestamp count for each user
     timestamps_data_counts = [
         (len(timestamps), key) for key, timestamps in scope.timestamps_data.items()
     ]
+    # sort by most timestamps
     timestamps_data_counts.sort(key=lambda x: x[0], reverse=True)
+    # user ids for top 1000 in num timestamps
     timestamps_data_keys = [key for _count, key in timestamps_data_counts[:1000]]
-    timestamps_data_result = []
+    # key, min, max for each user
+    scope.timestamps_data_result = [
+        f"{key}:{max(timestamps)}:{min(timestamps)}"
+        for key in timestamps_data_keys
+        if (timestamps := scope.timestamps_data[key])
+    ]
 
-    for key in timestamps_data_keys:
-        timestamps = scope.timestamps_data[key]
-        if timestamps:
-            highest = max(timestamps)
-            lowest = min(timestamps)
-            timestamps_data_result.append(f"{key}:{highest}:{lowest}")
-
-    user_finishes = {
+    # users with more than 10 finishes
+    scope.user_finishes = {
         key: [finishes[0], finishes[1], round(finishes[2], 2)]
         for key, finishes in scope.user_finishes.items()
         if finishes[0] >= 10
     }
-    user_finishes = dict(
-        sorted(user_finishes.items(), key=lambda x: x[1][0], reverse=True)[:200]
-    )
-
-    first_to_beat = {
-        key: beats for key, beats in scope.first_to_beat.items() if beats[1] >= 10
-    }
-    first_to_beat = dict(
-        sorted(first_to_beat.items(), key=lambda x: x[1][1], reverse=True)[:200]
+    # sort by finishes descending top 200
+    scope.user_finishes = dict(
+        sorted(scope.user_finishes.items(), key=lambda x: x[1][0], reverse=True)[:200]
     )
 
 
@@ -214,7 +181,6 @@ def main():
     utils.write_data(scope.sole_victors, "sole_victors")
     utils.write_data(scope.difficulty_records, "difficulty_records")
     utils.write_data(scope.difficulty_lengths, "difficulty_lengths")
-    utils.write_data(scope.first_to_beat, "first_to_beat")
     utils.write_data(scope.timestamps_data_result, "timestamps_data")
 
 
