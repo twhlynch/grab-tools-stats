1a04561b820ee5e648a3407391ebf1d96526f7ad
Tom Lynch <twhlynch@gmail.com>
2026-03-29 04:18:55 +1100

    refactor: clean up records scripts a bit

--- scripts/records.py
+++ scripts/records.py
@@ -1,126 +1,107 @@
 import sys
 import json
-import requests
-import contextlib
+import utils
 from concurrent.futures import ThreadPoolExecutor
 
 server_token_headers = json.loads(sys.argv[1])
 
-API = "https://api.slin.dev/grab/v1/"
 
-difficulty_records = {
-    "unrated": {},
-    "easy": {},
-    "medium": {},
-    "hard": {},
-    "veryhard": {},
-    "impossible": {},
-}
-difficulty_lengths = {
-    "unrated": 0,
-    "easy": 0,
-    "medium": 0,
-    "hard": 0,
-    "veryhard": 0,
-    "impossible": 0,
-    "total": 0,
-}
-leaderboard = {}
-sole_victors = []
-user_finishes = {}
-first_to_beat = {}
-timestamps_data = {}
+def get_level_leaderboard(identifier):
+    level_path = identifier.replace(":", "/")
+    url = f"{utils.SERVER_API}statistics_top_leaderboard/{level_path}"
 
+    response = utils.safe_get(url, server_token_headers)
+    data: list = utils.safe_json(response) or []
 
-def get_leaderboard(identifier):
-    identifier = identifier.replace(":", "/")
-
-    url = f"{API}statistics_top_leaderboard/{identifier}"
-
-    leaderboard = []
-    with contextlib.suppress(Exception):
-        leaderboard = requests.get(url, headers=server_token_headers).json()
-
-    filtered_leaderboard = [
-        entry for entry in leaderboard if "is_verification" not in entry
-    ]
-
-    length = len(filtered_leaderboard)
+    length = len(data)
     print(f"{length} entries for {identifier}")
 
-    return filtered_leaderboard
+    return data
 
 
-def save(filepath, data):
-    print(f"Saving {filepath}")
+class Scope:
+    def __init__(self):
+        # rating -> user id -> {maps, user_name}
+        self.difficulty_records = {rating: {} for rating in utils.RATINGS}
+        # rating -> level count
+        self.difficulty_lengths = {rating: 0 for rating in utils.RATINGS}
+        # user id -> [record count, ['title|identifier'][], username]
+        self.leaderboard = {}
+        # {...level, leaderboard}[]
+        self.sole_victors = []
+        # user id -> [finish count, username, total time]
+        self.user_finishes = {}
+        # user id -> [username, firsts count]
+        self.first_to_beat = {}
+        # user id -> timestamp[]
+        self.timestamps_data = {}
+        # 'user id|latest|oldest'[]
+        self.timestamps_data_result = []
+        # user id -> [record count, ['title|identifier'][], username]
+        self.sorted_leaderboard = {}
 
-    with open(f"stats_data/{filepath}.json", "w") as file:
-        json.dump(data, file)
 
+def process(level: dict, scope: Scope):
+    identifier = level["identifier"]
+    title = level.get("title", "")
+    statistics = level.get("statistics", {})
 
-def process_level(level):
-    title = level["title"] if "title" in level else ""
-    identifier = level["identifier"] if "identifier" in level else None
-    statistics = level["statistics"] if "statistics" in level else {}
+    leaderboard_data = get_level_leaderboard(identifier)
 
-    if identifier is None:
+    # ignore unbeaten maps
+    length = len(leaderboard_data)
+    if length == 0:
         return
 
-    print(f"Processing {title}")
+    # add leaderboard to level data
+    level["leaderboard"] = leaderboard_data
 
-    leaderboard_data = get_leaderboard(identifier)
+    # sole = only 1 record
+    if length == 1:
+        scope.sole_victors.append(level)
 
-    length = len(leaderboard_data)
+    # add record holder
+    record_holder = leaderboard_data[0]["user_id"]
+    if record_holder not in scope.leaderboard:
+        scope.leaderboard[record_holder] = [0, [], leaderboard_data[0]["user_name"]]
 
-    if length != 0:
-        level["leaderboard"] = leaderboard_data
+    scope.leaderboard[record_holder][0] += 1
+    scope.leaderboard[record_holder][1].append(
+        [title + "|" + identifier]
+    )  # TODO: why did i add the title here
 
-        # sole = only 1 record
-        if length == 1:
-            sole_victors.append(level)
+    # difficulty
+    difficulty_string = (
+        statistics["difficulty_string"]
+        if "difficulty_string" in statistics
+        else "unrated"
+    )
+    scope.difficulty_lengths[difficulty_string] += 1
 
-        # add record holder
-        record_holder = leaderboard_data[0]["user_id"]
-        if record_holder not in leaderboard:
-            leaderboard[record_holder] = [0, [], leaderboard_data[0]["user_name"]]
+    # finishes
+    for record in leaderboard_data:
+        user_id = record["user_id"] if "user_id" in record else None
+        timestamp = record["timestamp"] if "timestamp" in record else None
 
-        leaderboard[record_holder][0] += 1
-        leaderboard[record_holder][1].append(
-            [title + "|" + identifier]
-        )  # TODO: why did i add the title here
+        if user_id is None:
+            continue
 
-        # difficulty
-        difficulty_string = (
-            statistics["difficulty_string"]
-            if "difficulty_string" in statistics
-            else "unrated"
-        )
-        difficulty_lengths[difficulty_string] += 1
+        # timestamp data
+        if timestamp is not None:
+            timestamp_id = int(int(timestamp) / 100)
+            if user_id not in scope.timestamps_data:
+                scope.timestamps_data[user_id] = [timestamp_id]
+            else:
+                scope.timestamps_data[user_id].append(timestamp_id)
 
-        # finishes
-        for record in leaderboard_data:
-            user_id = record["user_id"] if "user_id" in record else None
-            timestamp = record["timestamp"] if "timestamp" in record else None
-
-            if user_id is None:
-                continue
-
-            # timestamp data
-            if timestamp is not None:
-                timestamp_id = int(int(timestamp) / 100)
-                if user_id not in timestamps_data:
-                    timestamps_data[user_id] = [timestamp_id]
-                else:
-                    timestamps_data[user_id].append(timestamp_id)
-
-            # difficulty records
-            diff_records = difficulty_records[difficulty_string]
-            if user_id not in diff_records:
-                diff_records[user_id] = {
-                    "maps": 0,
-                    "user_name": record["user_name"],
-                }
-            diff_records[user_id]["maps"] += 1
+        # difficulty records
+        diff_records = scope.difficulty_records[difficulty_string]
+        if user_id not in diff_records:
+            diff_records[user_id] = {
+                "maps": 0,
+                "user_name": record["user_name"],
+            }
+        diff_records[user_id]["maps"] += 1
 
     # first person to complete (in top 100)
     first_record = None
@@ -136,95 +117,106 @@ def process_level(level):
             first_record = record
 
         # total finishes
-        if user_id not in user_finishes:
-            user_finishes[user_id] = [0, record["user_name"], 0]
-        user_finishes[user_id][0] += 1
+        if user_id not in scope.user_finishes:
+            scope.user_finishes[user_id] = [0, record["user_name"], 0]
+        scope.user_finishes[user_id][0] += 1
 
         try:
-            user_finishes[user_id][2] += (
+            scope.user_finishes[user_id][2] += (
                 record["best_time"] if "best_time" in record else 0
             )
         except TypeError:  # FIXME: what caused this?
-            print(user_finishes[user_id])
+            print(scope.user_finishes[user_id])
 
     # first person to complete
     if first_record is not None:
         first_user_id = first_record["user_id"]
         first_user_name = first_record["user_name"]
 
-        if first_user_id not in first_to_beat:
-            first_to_beat[first_user_id] = [first_user_name, 0]
-        first_to_beat[first_user_id][1] += 1
-
-    print(f"Processed {title}")
+        if first_user_id not in scope.first_to_beat:
+            scope.first_to_beat[first_user_id] = [first_user_name, 0]
+        scope.first_to_beat[first_user_id][1] += 1
 
 
-with open("stats_data/all_verified.json") as file:
-    levels = json.load(file)
-
-difficulty_lengths["total"] = len(levels)
-
-# process all data
-with ThreadPoolExecutor() as executor:
-    futures = [executor.submit(process_level, level) for level in levels]
-
-    for future in futures:
-        future.result()
-
-# clean, sort, and slice
-leaderboard = {user: scores for user, scores in leaderboard.items() if scores[0] >= 10}
-sorted_leaderboard = dict(
-    sorted(leaderboard.items(), key=lambda x: x[1][0], reverse=True)
-)
-
-for difficulty in difficulty_records:
-    difficulty_records[difficulty] = {
-        user_key: records
-        for user_key, records in difficulty_records[difficulty].items()
-        if records["maps"] >= 10
+def sanitize(scope: Scope):
+    leaderboard = {
+        user: scores for user, scores in scope.leaderboard.items() if scores[0] >= 10
     }
+    scope.sorted_leaderboard = dict(
+        sorted(leaderboard.items(), key=lambda x: x[1][0], reverse=True)
+    )
 
-    difficulty_records[difficulty] = dict(
-        sorted(
-            difficulty_records[difficulty].items(),
-            key=lambda x: x[1]["maps"],
-            reverse=True,
-        )[:200]
+    for difficulty in scope.difficulty_records:
+        scope.difficulty_records[difficulty] = {
+            user_key: records
+            for user_key, records in scope.difficulty_records[difficulty].items()
+            if records["maps"] >= 10
+        }
+
+        scope.difficulty_records[difficulty] = dict(
+            sorted(
+                scope.difficulty_records[difficulty].items(),
+                key=lambda x: x[1]["maps"],
+                reverse=True,
+            )[:200]
+        )
+
+    timestamps_data_counts = [
+        (len(timestamps), key) for key, timestamps in scope.timestamps_data.items()
+    ]
+    timestamps_data_counts.sort(key=lambda x: x[0], reverse=True)
+    timestamps_data_keys = [key for _count, key in timestamps_data_counts[:1000]]
+    timestamps_data_result = []
+
+    for key in timestamps_data_keys:
+        timestamps = scope.timestamps_data[key]
+        if timestamps:
+            highest = max(timestamps)
+            lowest = min(timestamps)
+            timestamps_data_result.append(f"{key}:{highest}:{lowest}")
+
+    user_finishes = {
+        key: [finishes[0], finishes[1], round(finishes[2], 2)]
+        for key, finishes in scope.user_finishes.items()
+        if finishes[0] >= 10
+    }
+    user_finishes = dict(
+        sorted(user_finishes.items(), key=lambda x: x[1][0], reverse=True)[:200]
+    )
+
+    first_to_beat = {
+        key: beats for key, beats in scope.first_to_beat.items() if beats[1] >= 10
+    }
+    first_to_beat = dict(
+        sorted(first_to_beat.items(), key=lambda x: x[1][1], reverse=True)[:200]
     )
 
 
-timestamps_data_counts = [
-    (len(timestamps), key) for key, timestamps in timestamps_data.items()
-]
-timestamps_data_counts.sort(key=lambda x: x[0], reverse=True)
-timestamps_data_keys = [key for _count, key in timestamps_data_counts[:1000]]
-timestamps_data_result = []
+def main():
+    scope = Scope()
 
-for key in timestamps_data_keys:
-    timestamps = timestamps_data[key]
-    if timestamps:
-        highest = max(timestamps)
-        lowest = min(timestamps)
-        timestamps_data_result.append(f"{key}:{highest}:{lowest}")
+    # read levels
+    levels: list = utils.read_data("all_verified")
 
-user_finishes = {
-    key: [finishes[0], finishes[1], round(finishes[2], 2)]
-    for key, finishes in user_finishes.items()
-    if finishes[0] >= 10
-}
-user_finishes = dict(
-    sorted(user_finishes.items(), key=lambda x: x[1][0], reverse=True)[:200]
-)
+    # process all data
+    with ThreadPoolExecutor() as executor:
+        futures = [executor.submit(process, level, scope) for level in levels]
 
-first_to_beat = {key: beats for key, beats in first_to_beat.items() if beats[1] >= 10}
-first_to_beat = dict(
-    sorted(first_to_beat.items(), key=lambda x: x[1][1], reverse=True)[:200]
-)
+        for future in futures:
+            future.result()
 
-save("user_finishes", user_finishes)
-save("sorted_leaderboard_records", sorted_leaderboard)
-save("sole_victors", sole_victors)
-save("difficulty_records", difficulty_records)
-save("difficulty_lengths", difficulty_lengths)
-save("first_to_beat", first_to_beat)
-save("timestamps_data", timestamps_data_result)
+    # clean up, sort, slice
+    sanitize(scope)
+
+    # write stats
+    utils.write_data(scope.user_finishes, "user_finishes")
+    utils.write_data(scope.sorted_leaderboard, "sorted_leaderboard_records")
+    utils.write_data(scope.sole_victors, "sole_victors")
+    utils.write_data(scope.difficulty_records, "difficulty_records")
+    utils.write_data(scope.difficulty_lengths, "difficulty_lengths")
+    utils.write_data(scope.first_to_beat, "first_to_beat")
+    utils.write_data(scope.timestamps_data_result, "timestamps_data")
+
+
+if __name__ == "__main__":
+    main()
