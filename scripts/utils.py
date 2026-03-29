436cc9568453ea618960b415168afbf490e6b755
Tom Lynch <twhlynch@gmail.com>
2026-03-29 18:57:52 +1100

    refactor: better typing for utils

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,10 +1,90 @@
 import json
 import time
+from typing import Any, TypedDict
 
 import requests
 
-# type
+# types
 type JSON = dict[str, JSON] | list[JSON] | str | int | float | bool | None
+
+
+# full statistics
+class Statistics(TypedDict):
+    level_identifier: str
+    total_played_count: int
+    total_finished_count: int
+    played_count: int
+    finished_count: int
+    rated_count: int
+    liked_count: int
+    tipped_amount: int
+    tipped_count: int
+    average_time: float
+
+
+# statistics from level details
+class LevelStatistics(TypedDict):
+    total_played: int | None
+    difficulty: float | None
+    liked: float | None
+    time: float | None
+    difficulty_string: str | None
+
+
+def MakeLevelStatistics() -> LevelStatistics:
+    return {
+        "total_played": 0,
+        "difficulty": 1.0,
+        "liked": 0.0,
+        "time": 0.0,
+        "difficulty_string": "unrated",
+    }
+
+
+# level details from lists
+class Level(TypedDict):
+    identifier: str
+    iteration: int
+    data_key: str
+    complexity: int
+    title: str | None
+    description: str | None
+    creators: list[str] | None
+    tags: list[str] | None
+    verification_time: float | None
+    curated_listings: list[str] | None
+    format_version: int | None
+    update_timestamp: int | None
+    creation_timestamp: int | None
+    statistics: LevelStatistics | None
+    images: JSON
+
+
+# user details
+class User(TypedDict):
+    user_id: str
+    user_name: str | None
+    is_admin: bool
+    is_developer: bool
+    is_supermoderator: bool
+    is_moderator: bool
+    is_verifier: bool
+    is_creator: bool
+    user_level_count: int
+    grab_plus_active: bool
+    active_customizations: JSON
+
+
+# leaderboard placement entry
+class Placement(TypedDict):
+    best_time: float
+    position: int
+    timestamp: str
+    user_id: str
+    user_name: str
+    is_verification: bool | None
+    replay_key: str | None
+
 
 # api
 SERVER_API = "https://api.slin.dev/grab/v1/"
@@ -48,14 +128,14 @@ class Discord:
 
 
 # json files
-def write_data(data: JSON, name: str) -> None:
+def write_data(data: Any, name: str) -> None:
     with open(f"data/{name}.json", "w") as file:
         json.dump(data, file)
 
 
-def read_data(name: str) -> JSON:
+def read_data(name: str) -> Any:
     with open(f"data/{name}.json") as file:
-        data: JSON = json.load(file)
+        data = json.load(file)
 
     return data
 
@@ -86,7 +166,7 @@ def safe_get(
             time.sleep(delay)
 
 
-def safe_json(data: requests.Response | str | bytes | bytearray | None) -> JSON | None:
+def safe_json(data: requests.Response | str | bytes | bytearray | None) -> Any | None:
     if not data:
         return None
 
